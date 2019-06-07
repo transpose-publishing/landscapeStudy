d219e99a93f22481ba251de3d181793c1d0e8ddc
Thomas Klebel <t.klebel@hotmail.com>
2019-06-07 15:47:43 +0200

    explore more

--- analysis-scripts/02-explore.R
+++ analysis-scripts/02-explore.R
@@ -1,26 +1,59 @@
 library(tidyverse)
 theme_set(hrbrmisc::theme_hrbrmstr())
+library(RColorBrewer)
+library(ggalt)
+source("R/helpers.R")
 
-base_data <- read_csv("data-transformed/joined_data.csv")
+# import data
+refined <- read_csv("data-transformed/refined.csv")
+refined_with_areas <- read_csv("data-transformed/refined_w_areas.csv")
 
-refined <- base_data %>% 
-  mutate(pr_type_clean = case_when(
-    str_detect(pr_type, "^Double") ~ "double blind",
-    str_detect(pr_type, "^Other") ~ "other",
-    str_detect(pr_type, "^Single") ~ "single blind",
-    str_detect(pr_type, "^Not") ~ "not blinded",
-    TRUE ~ "unsure"
-  )) 
+# small tweaks
+refined_with_areas <- refined_with_areas %>% 
+  order_factors()
 
 refined %>% 
   count(pr_type_clean)
 
+# do some first explorations
 ggplot(refined, aes(pr_type_clean)) +
   geom_bar()
 
-ggplot(refined, aes(pr_type_clean)) +
-  geom_bar() +
-  facet_wrap()
+ggplot(refined_with_areas, aes(area, fill = area)) +
+  geom_bar(show.legend = F) +
+  facet_wrap(~pr_type_clean) +
+  coord_flip()
 
-# todo: create a set where the column "top journals in" is splitted (by ",")
-# with separate rows
+refined_with_areas %>% 
+  group_by(pr_type_clean) %>% 
+  count(area) %>% 
+  group_by(area) %>% 
+  mutate(prop = n/sum(n)) %>% 
+  ggplot(aes(area, prop, colour = area)) +
+  geom_lollipop(show.legend = F) +
+  facet_wrap(~pr_type_clean) +
+  coord_flip()
+
+ggplot(refined_with_areas, aes(area, fill = pr_type_clean)) +
+  geom_bar(position = "fill") +
+  # coord_flip() +
+  scale_fill_brewer(palette = "Set1")
+
+refined_with_areas %>% 
+  filter(is.na(area))
+
+ggplot(refined_with_areas, aes(area)) +
+  geom_bar()
+
+# explore h indices
+ggplot(refined_with_areas, aes(`h5-index`)) +
+  geom_histogram()
+
+ggplot(refined_with_areas, aes(pr_type_clean, `h5-index`)) +
+  geom_boxplot()
+
+ggplot(refined_with_areas, aes(area, `h5-index`)) +
+  geom_boxplot() +
+  coord_flip()
+
+refined_with_areas %>% arrange(desc(G_HMS_rank)) %>% select(G_100_rank, G_HMS_rank) %>% View()
