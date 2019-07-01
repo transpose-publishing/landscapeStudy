b3fdbd58b070314c84c3bfc840608df3f45275e3
Thomas Klebel <t.klebel@hotmail.com>
2019-07-01 12:09:55 +0200

    refactor test to still work

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
+library(landscapeStudy)
 
 
-test_that("data cleaning doesn't break", {
+test_that("data from excel is imported consistently", {
   transpose_clean <- read_excel("TRANSPOSE landscape study - 2019-06-02.xlsx",
                                 sheet = "Raw") %>%
     clean_raw_sheet(source = "excel")
@@ -13,15 +14,13 @@ test_that("data cleaning doesn't break", {
   # relevant. info on publisher needs to be removed, since this was the reason for
   # the whole exercise (having missing publishers)
   transpose_test <- transpose_clean %>%
-    select(-starts_with("review_date"), -publisher)
+    select(-starts_with("review"), -publisher)
 
   # import filtered data and do some renaming
   transpose_raw <- read_excel("TRANSPOSE landscape study - 2019-06-02.xlsx",
                               sheet = "Filtered") %>%
-    select(-starts_with("To dis")) %>%
-    select(-`review date...2`, -`review date...4`, -`review 3 date`,
-           top_journals_in = starts_with("Top journals"),
-           -publisher)
+    select(-starts_with("To dis"), -publisher, -starts_with("review")) %>%
+    rename(top_journals_in = starts_with("Top journals"))
 
 
   old_names <- names(transpose_raw)
