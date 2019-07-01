b1ac9748e41f5af99e4a82ece652b16047938022
Thomas Klebel <t.klebel@hotmail.com>
2019-07-01 11:14:42 +0200

    remove trailing WS

--- analysis-scripts/01-import.R
+++ analysis-scripts/01-import.R
@@ -11,11 +11,11 @@ source("R/helpers.R")
 gs_dat <- read_excel("data-raw/Compiled_Landscape study journals list.xlsx",
                      sheet = "G_ALL_dedup")
 
-gs_dat <- select(gs_dat, -contains("Reviewer"), issn = ISSN, 
+gs_dat <- select(gs_dat, -contains("Reviewer"), issn = ISSN,
                  gs_title = `Journal Title`)
 
 # fix error for American Journal of Political Science: rank was missing
-gs_dat <- gs_dat %>% 
+gs_dat <- gs_dat %>%
   mutate(G_SS_rank = case_when(
     issn == "1540-5907" ~ 8,
     TRUE ~ G_SS_rank)
@@ -26,7 +26,7 @@ gs_dat <- gs_dat %>%
 # so we have to import the raw sheet, and then take either the first or the
 # third row.
 transpose_clean <- read_excel("data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
-                        sheet = "Raw") %>% 
+                        sheet = "Raw") %>%
   clean_raw_sheet()
 
 
@@ -37,16 +37,16 @@ transpose_clean <- read_excel("data-raw/TRANSPOSE landscape study - 2019-06-02.x
 # there are some annoying columns with inconsistent date formats, which are not
 # relevant. info on publisher needs to be removed, since this was the reason for
 # the whole exercise (having missing publishers)
-transpose_test <- transpose_clean %>% 
+transpose_test <- transpose_clean %>%
   select(-starts_with("review_date"), -publisher)
 
 # import filtered data and do some renaming
 transpose_raw <- read_excel("data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
-                        sheet = "Filtered") %>% 
-  select(-starts_with("To dis")) %>% 
+                        sheet = "Filtered") %>%
+  select(-starts_with("To dis")) %>%
   select(-`review date...2`, -`review date...4`, -`review 3 date`,
          top_journals_in = starts_with("Top journals"),
-         -publisher) 
+         -publisher)
 
 
 old_names <- names(transpose_raw)
@@ -67,7 +67,7 @@ write_csv(transpose_clean, "data-transformed/before_manual_fixing.csv")
 # What will be fixed:
 # - missing publisher
 # - missing data in other places
-# 
+#
 # diffing can be done via:
 # git diff --color-words="[^[:space:],]+" *after*
 # git diff --color-words="[^[:space:],]+" 16ad54bf e1e2b554
@@ -83,42 +83,42 @@ transpose_fixed <- read_csv("data-transformed/after_manual_fixing.csv")
 # Merge datasets -----
 # two issn's are problematic:
 # - for "Neural Information Processing Systems (NIPS)" there is no issn
-# - for "Annals of the Rheumatic Diseases", once online and once print issn 
+# - for "Annals of the Rheumatic Diseases", once online and once print issn
 # where used.
 
 
 # steps to solve:
 # - dummy issn for NIPS
 # - change issn to online version
-gs_dat <- gs_dat %>% 
+gs_dat <- gs_dat %>%
   mutate(issn = case_when(str_detect(gs_title, "Rheuma") ~ "1468-2060",
                           str_detect(gs_title, "NIPS") ~ "NIPS-ISSN",
                           TRUE ~ issn))
 
-transpose_fixed <- transpose_fixed %>% 
+transpose_fixed <- transpose_fixed %>%
   mutate(issn = case_when(str_detect(title, "NIPS") ~ "NIPS-ISSN",
                           TRUE ~ issn))
 
 
-joined_dataset <- transpose_fixed %>% 
+joined_dataset <- transpose_fixed %>%
   full_join(gs_dat)
 
 
 # check if matching went right -----
-check1 <- transpose_fixed %>% 
-  anti_join(gs_dat, by = "issn") %>% 
-  nrow() 
+check1 <- transpose_fixed %>%
+  anti_join(gs_dat, by = "issn") %>%
+  nrow()
 
-check2  <- gs_dat %>% 
-  anti_join(transpose_fixed, by = "issn") %>% 
+check2  <- gs_dat %>%
+  anti_join(transpose_fixed, by = "issn") %>%
   nrow()
 
 if (any(check1 != 0, check2 != 0)) {
   stop("Matching wasn't sucessful! Please check the output!")
 } else {
-  # write result to disk 
+  # write result to disk
   write_csv(joined_dataset, "data-transformed/joined_data.csv")
-  
+
 }
 
 
@@ -126,19 +126,19 @@ if (any(check1 != 0, check2 != 0)) {
 transpose_vars <- read_excel(
   "data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
   sheet = "Raw", n_max = 2)
-  
-part1 <- transpose_vars %>% 
-  slice(1) %>% 
-  gather(variable, description) %>% 
+
+part1 <- transpose_vars %>%
+  slice(1) %>%
+  gather(variable, description) %>%
   slice(-1)
 
-part2 <- transpose_vars %>% 
-  slice(2) %>% 
-  gather(variable, type) %>% 
+part2 <- transpose_vars %>%
+  slice(2) %>%
+  gather(variable, type) %>%
   slice(-1)
 
-part1 %>% 
-  left_join(part2) %>% 
+part1 %>%
+  left_join(part2) %>%
   mutate(var_number = 1:n())   -> var_overview
 
 write_csv(var_overview, "data-transformed/var_overview.csv")
