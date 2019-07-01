2355538a6c0a66ac3e760215f28f67e9bfcee43c
Thomas Klebel <t.klebel@hotmail.com>
2019-07-01 16:14:20 +0200

    rm trailing WS

--- analysis-scripts/03-augment.R
+++ analysis-scripts/03-augment.R
@@ -1,26 +1,25 @@
 library(tidyverse)
-theme_set(hrbrmisc::theme_hrbrmstr())
 
 # import data
 base_data <- read_csv("data-transformed/joined_data.csv")
 
 # create clean variable for peer review type
-refined <- base_data %>% 
+refined <- base_data %>%
   mutate(pr_type_clean = case_when(
     str_detect(pr_type, "^Double") ~ "double blind",
     str_detect(pr_type, "^Other") ~ "other",
     str_detect(pr_type, "^Single") ~ "single blind",
     str_detect(pr_type, "^Not") ~ "not blinded",
     TRUE ~ "unsure"
-  )) 
+  ))
 
 
 # # check publisher names
 # refined %>%
-#   count(publisher) %>% 
+#   count(publisher) %>%
 #   arrange(publisher)
 
-refined <- refined %>% 
+refined <- refined %>%
   mutate(publisher_clean = case_when(
     str_detect(publisher, "^American Chemical Society") ~ "American Chemical Society",
     str_detect(publisher, "^American Heart Association") ~ "American Heart Association",
@@ -37,33 +36,33 @@ refined <- refined %>%
 # we first create a variable with the full area names. then we squash it into
 # one row and tuck it onto the original data frame.
 
-recoded <- refined %>% 
-  select(issn, starts_with("G_"), -G_100_rank) %>% 
-  gather(area, score, -issn) %>% 
-  drop_na() %>% 
+recoded <- refined %>%
+  select(issn, starts_with("G_"), -G_100_rank) %>%
+  gather(area, score, -issn) %>%
+  drop_na() %>%
   mutate(area_clean = case_when(
     str_detect(area, "BEM") ~ "Business, Economics & Management",
-    str_detect(area, "CMS") ~ "Chemical & Materials Sciences", 
+    str_detect(area, "CMS") ~ "Chemical & Materials Sciences",
     str_detect(area, "ECS") ~ "Engineering & Computer Science",
     str_detect(area, "HMS") ~ "Health & Medical Sciences",
-    str_detect(area, "HLA") ~ "Humanities, Literature & Arts", 
+    str_detect(area, "HLA") ~ "Humanities, Literature & Arts",
     str_detect(area, "LSES") ~ "Life Sciences & Earth Sciences",
     str_detect(area, "PM") ~ "Physics & Mathematics",
     str_detect(area, "SS") ~ "Social Sciences"
-  )) %>% 
-  select(issn, area = area_clean) %>% 
-  arrange(issn) 
+  )) %>%
+  select(issn, area = area_clean) %>%
+  arrange(issn)
 
-subject_areas <- recoded %>% 
-  group_by(issn) %>% 
-  mutate(area_combined = list(unique(area))) %>% 
-  slice(1) %>% 
+subject_areas <- recoded %>%
+  group_by(issn) %>%
+  mutate(area_combined = list(unique(area))) %>%
+  slice(1) %>%
   select(-area, area = area_combined)
 
-refined_with_areas <- refined %>% 
-  left_join(subject_areas) %>% 
-  filter(!map_lgl(area, is.null)) %>% 
-  unnest() %>% 
+refined_with_areas <- refined %>%
+  left_join(subject_areas) %>%
+  filter(!map_lgl(area, is.null)) %>%
+  unnest() %>%
   right_join(refined)
 
 # write augmented datasets to disk
