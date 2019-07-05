ba3a2929c77fee3f69a9fe3cfc0666c40fba6c4b
Thomas Klebel <t.klebel@hotmail.com>
2019-07-05 15:39:42 +0200

    import data from google sheets (gs) with correct files
    Now we are using the correct and original files

--- analysis-scripts/01-import.R
+++ analysis-scripts/01-import.R
@@ -3,13 +3,13 @@
 # - data from the landscape study, which should then be analysed
 
 library(tidyverse)
-library(readxl)
 library(janitor)
 library(landscapeStudy)
+library(googlesheets)
 
 # Import GS data ----
-gs_dat <- read_excel("data-raw/Compiled_Landscape study journals list.xlsx",
-                     sheet = "G_ALL_dedup")
+gsm_sheet <- gs_title("Compiled_Landscape study journals list")
+gs_dat <- gs_read(gsm_sheet, ws = "G_ALL_dedup")
 
 gs_dat <- select(gs_dat, -contains("Reviewer"), issn = ISSN,
                  gs_title = `Journal Title`)
@@ -25,60 +25,11 @@ gs_dat <- gs_dat %>%
 # we are re-doing the import which was done by Jessica inside the sheet.
 # so we have to import the raw sheet, and then take either the first or the
 # third row.
-transpose_clean <- read_excel("data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
-                        sheet = "Raw", .name_repair = "universal") %>%
-  clean_raw_sheet(source = "excel")
+ls_sheet <- gs_title("TRANSPOSE landscape study - round 3")
 
-
-
-# just to check, whether there are any changes, we import the Filtered sheet as
-# well and check if they are identical
-
-# there are some annoying columns with inconsistent date formats, which are not
-# relevant. info on publisher needs to be removed, since this was the reason for
-# the whole exercise (having missing publishers)
-transpose_test <- transpose_clean %>%
-  select(-starts_with("review_date"), -publisher)
-
-# import filtered data and do some renaming
-transpose_raw <- read_excel("data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
-                        sheet = "Filtered", .name_repair = "universal") %>%
-  select(-starts_with("To dis")) %>%
-  select(-`review.date...2`, -`review.date...4`, -`review.3.date`,
-         top_journals_in = starts_with("Top.journals"),
-         -publisher)
-
-
-old_names <- names(transpose_raw)
-new_names <- str_replace_all(old_names, "\\.", "_")
-names(transpose_raw) <- new_names
-
-if (identical(transpose_test, transpose_raw)) {
-  "Everything is fine!"
-} else {
-  cat(all.equal(transpose_test, transpose_raw), "\n")
-  stop("There is a problem here!!", call. = FALSE)
-}
-
-
-# export the newly filtered data as csv
-write_csv(transpose_clean, "data-transformed/before_manual_fixing.csv")
-
-
-# What will be fixed:
-# - missing publisher
-# - missing data in other places
-#
-# diffing can be done via:
-# git diff --color-words="[^[:space:],]+" *after*
-# git diff --color-words="[^[:space:],]+" 16ad54bf e1e2b554
-# git diff --no-index --color-words="[^[:space:],]+" before_manual_fixing.csv after_manual_fixing.csv
-# the last one is not very helpful since quoting changes a lot of things...
-# --no-index is needed, since these are two uncommited files
-
-transpose_fixed <- read_csv("data-transformed/before_manual_fixing.csv")
-
-
+transpose_fixed <- gs_read(ls_sheet) %>%
+  as_tibble(.name_repair = "universal") %>%
+  clean_raw_sheet(source = "google")
 
 
 # Merge datasets -----
@@ -124,9 +75,9 @@ if (any(check1 != 0, check2 != 0)) {
 
 
 # import list of variables for ease of use
-transpose_vars <- read_excel(
-  "data-raw/TRANSPOSE landscape study - 2019-06-02.xlsx",
-  sheet = "Raw", n_max = 2)
+transpose_vars <- gs_read(
+  ls_sheet,
+  ws = "Raw", n_max = 2)
 
 part1 <- transpose_vars %>%
   slice(1) %>%
