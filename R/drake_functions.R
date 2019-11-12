333961da6508e57401c487599140be1ea08a1839
Thomas Klebel <t.klebel@hotmail.com>
2019-11-12 14:47:37 +0100

    add drake functions for augmentation

--- R/drake_functions.R
+++ R/drake_functions.R
@@ -1,6 +1,5 @@
 # data import functions ------
 
-
 import_raw_data <- function(out_file) {
   # Import data on google scholar rankings of journals ----
   gsm_sheet <- gs_title("Compiled_Landscape study journals list")
@@ -111,4 +110,124 @@ create_var_overview <- function(out_file) {
     mutate(var_number = 1:n())   -> var_overview
 
   write_csv(var_overview, out_file)
+}
+
+recode_vars <- function(raw_data, oa_data, out_path) {
+  # import data
+  base_data <- read_csv(
+    raw_data,
+    col_types = cols(
+      .default = col_character(),
+      review_date_1 = col_character(),
+      review_date_2 = col_character(),
+      review_date_3 = col_character(),
+      `h5-index` = col_double(),
+      `h5-median` = col_double(),
+      G_100_rank = col_double(),
+      G_BEM_rank = col_double(),
+      G_CMS_rank = col_double(),
+      G_ECS_rank = col_double(),
+      G_HMS_rank = col_double(),
+      G_HLA_rank = col_double(),
+      G_LSES_rank = col_double(),
+      G_PM_rank = col_double(),
+      G_SS_rank = col_double()
+    ))
+
+  # create clean variable for peer review type
+  refined <- base_data %>%
+    mutate(pr_type_clean = case_when(
+      str_detect(pr_type, "^Double") ~ "Double blind",
+      str_detect(pr_type, "^Other") ~ "Other",
+      str_detect(pr_type, "^Single") ~ "Single blind",
+      str_detect(pr_type, "^Not") ~ "Not blinded",
+      TRUE ~ "Unsure"
+    ))
+
+
+  # # check publisher names
+  # refined %>%
+  #   count(publisher) %>%
+  #   arrange(publisher)
+
+  refined <- refined %>%
+    mutate(publisher_clean = case_when(
+      str_detect(publisher, "^American Chemical Society") ~ "American Chemical Society",
+      str_detect(publisher, "^American Heart Association") ~ "American Heart Association",
+      str_detect(publisher, "^Cell") ~ "Cell Press (Elsevier)",
+      str_detect(publisher, "^IEEE") ~ "IEEE",
+      str_detect(publisher, "^IOP") ~ "IOP",
+      str_detect(publisher, "^SpringerNature") ~ "Springer Nature",
+      TRUE ~ publisher
+    ))
+
+
+  # fix missings for coreview_policy
+  refined <- refined %>%
+    mutate(coreview_policy = fct_explicit_na(coreview_policy, "Not specified"))
+
+
+
+  # add data on open access status
+  oa_status <- read_csv(here::here(oa_data),
+                        col_types = cols(
+                          issn = col_character(),
+                          bibjson.title = col_character(),
+                          bibjson.publisher = col_character(),
+                          bibjson.oa_start.year = col_double(),
+                          issn_type = col_character()
+                        ))
+
+  refined %>%
+    left_join(oa_status, by = "issn") %>%
+    write_csv(out_path)
+}
+
+recode_to_areas <- function(refined, missing_categories, out_path) {
+
+  recoded <- refined %>%
+    select(issn, starts_with("G_"), -G_100_rank) %>%
+    gather(area, score, -issn) %>%
+    drop_na() %>%
+    mutate(area_clean = case_when(
+      str_detect(area, "BEM") ~ "Business, Economics & Management",
+      str_detect(area, "CMS") ~ "Chemical & Materials Sciences",
+      str_detect(area, "ECS") ~ "Engineering & Computer Science",
+      str_detect(area, "HMS") ~ "Health & Medical Sciences",
+      str_detect(area, "HLA") ~ "Humanities, Literature & Arts",
+      str_detect(area, "LSES") ~ "Life Sciences & Earth Sciences",
+      str_detect(area, "PM") ~ "Physics & Mathematics",
+      str_detect(area, "SS") ~ "Social Sciences"
+    )) %>%
+    select(issn, area = area_clean) %>%
+    arrange(issn)
+
+
+  refined_with_areas <- refined %>%
+    full_join(recoded, by = "issn")
+
+  # data on areas for some journals was not included. here we import the scraped
+  # data and join it with the rest
+  hand_coded_areas <- read_csv(
+    missing_categories,
+    col_types = cols(
+      title = col_character(),
+      issn = col_character(),
+      area = col_character(),
+      id = col_character(),
+      `h5-index` = col_double(),
+      `h5-median` = col_double()
+    )) %>%
+    select(issn, area) %>%
+    mutate(area_was_scraped = TRUE)
+
+  refined_with_areas <- refined_with_areas %>%
+    full_join(hand_coded_areas, by = "issn") %>%
+    mutate(area = coalesce(area.x, area.y)) %>%
+    select(-area.y, -area.x) %>%
+    replace_na(list(area_was_scraped = FALSE))
+
+
+
+  write_csv(refined_with_areas, out_path)
 }
--- R/plan.R
+++ R/plan.R
@@ -5,5 +5,14 @@ plan <- drake_plan(
   variables = create_var_overview(
     out_file = file_out("data-transformed/var_overview.csv")
   ),
-
+  data_recoded = recode_vars(
+    raw_data = file_in("data-transformed/joined_data.csv"),
+    oa_data = file_in("data-transformed/oa_data.csv"),
+    out_path = file_out("data-transformed/refined.csv")
+  ),
+  data_with_areas = recode_to_areas(
+    refined = data_recoded,
+    missing_categories = file_in("data-transformed/journals_with_missing_categories.csv"),
+    out_path = file_out("data-transformed/refined_w_areas.csv")
+  )
 )
