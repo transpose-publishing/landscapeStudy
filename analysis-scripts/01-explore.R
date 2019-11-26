adc22df1678a26e5d2dfb096cb2d771d6f818d53
Thomas Klebel <t.klebel@hotmail.com>
2019-11-26 12:16:46 +0100

    fix function name
    the function name was changed in de932b1d9e021738c72e48f0a8240c54c9f801e8 but not changed in the other files

--- analysis-scripts/01-explore.R
+++ analysis-scripts/01-explore.R
@@ -10,10 +10,10 @@ refined <- read_csv("data-transformed/refined.csv")
 refined_with_areas <- read_csv("data-transformed/refined_w_areas.csv")
 
 # small tweaks
-refined_with_areas <- refined_with_areas %>% 
-  order_factors()
+refined_with_areas <- refined_with_areas %>%
+  order_pr_type()
 
-refined %>% 
+refined %>%
   count(pr_type_clean)
 
 # do some first explorations
@@ -25,11 +25,11 @@ ggplot(refined_with_areas, aes(area, fill = area)) +
   facet_wrap(~pr_type_clean) +
   coord_flip()
 
-refined_with_areas %>% 
-  group_by(pr_type_clean) %>% 
-  count(area) %>% 
-  group_by(area) %>% 
-  mutate(prop = n/sum(n)) %>% 
+refined_with_areas %>%
+  group_by(pr_type_clean) %>%
+  count(area) %>%
+  group_by(area) %>%
+  mutate(prop = n/sum(n)) %>%
   ggplot(aes(area, prop, colour = area)) +
   geom_lollipop(show.legend = F) +
   facet_wrap(~pr_type_clean) +
@@ -42,7 +42,7 @@ ggplot(refined_with_areas, aes(str_wrap(area, 20), fill = pr_type_clean)) +
   theme(legend.position = "top") +
   labs(fill = NULL)
 
-refined_with_areas %>% 
+refined_with_areas %>%
   filter(is.na(area))
 
 ggplot(refined_with_areas, aes(area)) +
@@ -59,29 +59,29 @@ ggplot(refined_with_areas, aes(area, `h5-index`)) +
   geom_boxplot() +
   coord_flip()
 
-refined_with_areas %>% 
-  arrange(desc(G_HMS_rank)) %>% 
-  select(G_100_rank, G_HMS_rank) %>% 
+refined_with_areas %>%
+  arrange(desc(G_HMS_rank)) %>%
+  select(G_100_rank, G_HMS_rank) %>%
   View()
 
 
 ## enrich with data about n of articles from crossref ----
 # take first issn
-test_issn <- refined %>% 
-  head(1) %>% 
+test_issn <- refined %>%
+  head(1) %>%
   pull(issn)
 
 test_journal <- cr_journals(test_issn, works = T)
 
-test_journal$data %>% 
+test_journal$data %>%
   View()
 
 test_journal_big <- cr_journals(test_issn, works = T, limit = 1000,
-                                filter = c(from_pub_date = 2017, 
+                                filter = c(from_pub_date = 2017,
                                            until_pub_date = 2018))
 
-test_journal_big$data %>% 
-  as.tibble() %>% 
+test_journal_big$data %>%
+  as.tibble() %>%
   mutate(reference.count = as.integer(reference.count)) %>% View()
 
 test_journal_very_big <- cr_journals(
@@ -90,6 +90,6 @@ test_journal_very_big <- cr_journals(
   filter = c(from_pub_date = 2015, until_pub_date = 2018),
   .progress = TRUE)
 
-test_journa_very_big$data %>% 
+test_journa_very_big$data %>%
   as.tibble() %>% View()
 
