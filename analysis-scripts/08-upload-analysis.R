ec385999875839a4cf0b0e49fcf07fdb41e8b6e9
Thomas Klebel <t.klebel@hotmail.com>
2019-12-11 11:03:28 +0100

    add comment on how to execute uploading the file to google drive

--- analysis-scripts/08-upload-analysis.R
+++ analysis-scripts/08-upload-analysis.R
@@ -1,6 +1,14 @@
 # For uploading to google drive, the following function was used.
+#
+# The following code block needs to be inserted into the second chunk (import)
+# at the top:
+# source("R/packages.R")
+# source("R/correspondence_analysis_funs.R")
+# source("R/helpers.R")
+# source("R/plotting_functions.R")
+# source("R/drake_functions.R")
 
 # devtools::install_github("MilesMcBain/markdrive")
 
-markdrive::gdoc_render("analysis-scripts/12-analysis-writeup.Rmd",
+markdrive::gdoc_render("02-analysis-writeup.Rmd",
                        "analysis-writeup")
