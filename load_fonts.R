61bf1a85cac5db81ad9d3a209c349ebabc076d91
Thomas Klebel <t.klebel@hotmail.com>
2020-01-17 10:33:11 +0100

    document stepts to load fonts

---
+++ load_fonts.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(extrafont)
+
+# download fonts from https://fonts.google.com/?selection.family=Hind
+# install fonts manually
+# register fonts with extrafont
+extrafont::font_import(paths = "c:/Users/ThomasKlebel/AppData/Local/Microsoft/Windows/Fonts/")
+
+# depending on the device you are using, you might need to load them explicietly
+extrafont::loadfonts(device = "win")
+# or
+extrafont::loadfonts()
+
+
+# try it out
+ggplot(mtcars, aes(mpg)) +
+  geom_histogram() +
+  theme_bw(base_family = "Hind")
+
+
+# this may work to automatically detect the device to load font if on windows
+# if (.Platform$OS.type == "windows") extrafont::loadfonts(device = "win")
