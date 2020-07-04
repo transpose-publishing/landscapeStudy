8be3f21d4215451ac16c6a32bc7e6c4cb4f03a1d
Thomas Klebel <t.klebel@hotmail.com>
2020-07-04 16:27:56 +0200

    document manual changes to dependency graph file

--- documentation/create_analysis_pipeline_vis.R
+++ documentation/create_analysis_pipeline_vis.R
@@ -7,3 +7,8 @@ res <- drake_graph_info(plan)
 res$nodes <- filter(res$nodes, !str_detect(label, "::"))
 
 render_drake_graph(res, file = "documentation/analysis_pipeline.html")
+
+# two tweaks were added manually to the html file before taking a screenshot:
+# 1. the colour was changed to "#228B22" for all nodes that were out of date
+# 2. the script for the html-widget was changed so the display would be wider:
+# `"browser":{"width":1800,"height":800,"padding":40,"fill":false`
