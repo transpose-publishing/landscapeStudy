45211905521380dbaa2fb146ccb4c377b1b853e3
Thomas Klebel <t.klebel@hotmail.com>
2020-07-04 16:23:31 +0200

    add file with the analysis pipeline

---
+++ documentation/create_analysis_pipeline_vis.R
@@ -0,0 +1,9 @@
+library(drake)
+library(dplyr)
+source("R/plan.R")
+res <- drake_graph_info(plan)
+
+# remove some function calls
+res$nodes <- filter(res$nodes, !str_detect(label, "::"))
+
+render_drake_graph(res, file = "analysis_pipeline.html")
