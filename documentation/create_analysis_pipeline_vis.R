5f9517918c5f59242c35dfd92ecdc5470f49bc32
Thomas Klebel <t.klebel@hotmail.com>
2020-07-05 10:55:02 +0200

    update analysis pipeline accordingly

--- documentation/create_analysis_pipeline_vis.R
+++ documentation/create_analysis_pipeline_vis.R
@@ -1,12 +1,16 @@
 library(drake)
-library(dplyr)
+source("R/packages.R")
+source("R/correspondence_analysis_funs.R")
+source("R/helpers.R")
+source("R/plotting_functions.R")
+source("R/drake_functions.R")
 source("R/plan.R")
 res <- drake_graph_info(plan)
 
 # remove some function calls
 res$nodes <- filter(res$nodes, !str_detect(label, "::"))
 
-render_drake_graph(res, file = "documentation/analysis_pipeline.html")
+render_drake_graph(res, file = "analysis_pipeline.html")
 
 # two tweaks were added manually to the html file before taking a screenshot:
 # 1. the colour was changed to "#228B22" for all nodes that were out of date
